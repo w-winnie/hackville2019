2d1502edf54fcb0a69649b70f8a4d42eed279ee5
Winnie <aqua.fieryflakes@gmail.com>
2019-02-10 06:17:56 -0500

    Final Prototype

--- src/controller/host/ViewProfileController.java
+++ src/controller/host/ViewProfileController.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.GuestBean;
+import dal.DB_Access;
 
 @WebServlet("/ViewProfileController")
 public class ViewProfileController extends HttpServlet {
